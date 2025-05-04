af854d85e4dcba9e4c7b7b860e983566f3e53e56
Thirumalareddy Siddavatam <thirumalareddy.sidda@FALVMFAA09.fractal.com>
2025-05-04 22:00:15 +0530

    second commit

--- train/train_model.py
+++ train/train_model.py
@@ -27,7 +27,7 @@ y = df['Reached.on.Time_Y.N']
 
 # Train model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = RandomForestClassifier(random_state=42)
+model = RandomForestClassifier(random_state=50)
 model.fit(X_train, y_train)
 
 # Save model
